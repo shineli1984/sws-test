568a2e59bf3dc7585477233cc8b5185dd39bb667
Shine Li <shine.li@digitalasset.com>
2020-06-16 16:47:47 +1000

    remove tests for raw sql given type safe code for querying is in place

--- test/Persistence/QueriesSpec.hs
+++ test/Persistence/QueriesSpec.hs
@@ -15,38 +15,5 @@ instance Arbitrary SortingDirection where
 
 spec :: Spec
 spec = do
-  describe "ToRawSql instances" $ do
-    context "sorting direction" $ do
-      it "produces sql direction" $ property $
-        \(direction :: SortingDirection) ->
-          sql direction `shouldBe` (toUpper . pack . show $ direction)
-
-    context "Sorting Score" $ do
-      it "produces 'order by score'" $ property $
-        \(direction :: SortingDirection) ->
-          sql (Score direction) `shouldBe` ("order by score " <> sql direction)
-
-    context "filter" $ do
-      it "turns Symbol into condition" $ property $
-        \(sym :: String) ->
-          sql (SymbolFilter . pack $ sym) `shouldBe` "swsCompany.exchange_symbol = ?"
-      it "turns Score into condition" $ property $
-        \(score :: Int64) ->
-          sql (ScoreFilter score) `shouldBe` "score >= ?"
-
-      it "turns both Symbol & Score filters into condition" $ property $
-        \(sym :: [String], score :: Int64) -> do
-          let actual = sql (SymbolFilter . pack <$> sym, Just . ScoreFilter $ score) 
-          case sym of
-            [] -> actual `shouldBe` "where score >= ?"
-            syms -> actual `shouldBe` "where (" <> intercalate " OR " ((\_sym -> ("swsCompany.exchange_symbol = ?")) <$> syms) <> ") AND score >= ?"
-              
-  
-  describe "ToParam instances" $ do
-    context "filtering" $ do
-      it "turns Symbol into list of PersistText" $ property $
-        \(sym :: String) ->
-          param (SymbolFilter . pack $ sym) `shouldBe` [PersistText . pack $ sym]
-      it "turns Score into list of PersistInt64" $ property $
-        \(score :: Int64) ->
-          param (ScoreFilter score) `shouldBe` [PersistInt64 score]
+    pure ()
+ 
